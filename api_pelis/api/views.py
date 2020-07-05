32ec76adc3a52de24c5bdc55aab186c785d4918c
Axel Eleazar Garcia Darce <agarciadarce@gmail.com>
2020-07-04 18:11:18 -0600

    creando api :D

--- api_pelis/api/views.py
+++ api_pelis/api/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .models import Pelicula
+from .serializers import PeliculaSerializer
+from rest_framework import viewsets
+
+class PeliculaViewSet(viewsets.ModelViewSet):
+  queryset = Pelicula.objects.all()
+  serializer_class = PeliculaSerializer
